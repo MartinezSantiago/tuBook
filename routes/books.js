911c2a3f25d3baa1d0e3341eb7970691c7db3707
Santiago Martinez <79525736+MartinezSantiago@users.noreply.github.com>
2023-05-20 15:11:32 -0300

    Add files via upload

--- routes/books.js
+++ routes/books.js
@@ -4,26 +4,43 @@ const router = express.Router();
 router.get('/', (req, res) => {
   const connection = req.dbConnection;
   const query = "SELECT * FROM books";
-
   connection.query(query, (err, results) => {
     if (err) {
       console.error('Error executing MySQL query:', err);
       res.status(500).json({ error: 'Failed to retrieve books from the database' });
       return;
     }
-
     res.json(results);
   });
 });
 
 router.get('/:id', (req, res) => {
   const bookId = req.params.id;
-  res.send(`Book ID: ${bookId}`);
+  const connection = req.dbConnection;
+  const query = "SELECT * FROM books where id="+bookId;
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ error: 'Failed to retrieve books from the database' });
+      return;
+    }
+    res.json(results);
+  });
 });
 
 router.post('/', (req, res) => {
   // Logic for creating a new book
-  res.send('Create a new book');
+  const connection = req.dbConnection;
+  const { title, author, rating, publication_year, genre, price } = req.body;
+  const query = `INSERT INTO books (title, author, rating, publication_year, genre, price) VALUES ('${title}', '${author}', ${rating}, ${publication_year}, '${genre}', ${price});`;
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ error: 'Failed to retrieve books from the database' });
+      return;
+    }
+    res.json("Se creo el libro correctamente");
+  });
 });
 
 router.put('/:id', (req, res) => {
