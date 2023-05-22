8443f27d2b94ac680a9c49012b61e8647ad93241
Santiago Martinez <79525736+MartinezSantiago@users.noreply.github.com>
2023-05-22 19:31:23 -0300

    Add files via upload

--- routes/images.js
+++ routes/images.js
@@ -6,11 +6,23 @@ const upload = multer();
 router.post("/", upload.single('image'), async (req, res) => {
     const image = req.file; 
     try {
-      await req.uploadImageToS3("tubookv1", "test2.jpg", image.buffer);
-      res.json("Image uploaded successfully");
+      const link=await req.uploadImageToS3("tubookv1", req.file.filename+"_+"+datetimeToString()+".jpg", image.buffer);
+      res.json(link);
     } catch (error) {
       console.error('Error uploading image:', error);
       res.status(500).json("Error uploading image");
     }
   });
+
+function datetimeToString() {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  const hours = String(currentDate.getHours()).padStart(2, '0');
+  const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+  const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+  return `${year}${month}${day}${hours}${minutes}${seconds}`;
+}
 module.exports=router
