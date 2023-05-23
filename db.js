3983be5546319f1b3bf9e67891eaa7aa27233462
Santiago Martinez <79525736+MartinezSantiago@users.noreply.github.com>
2023-05-23 11:38:05 -0300

    Update db.js

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 function createConnection() {
   const connection = mysql.createConnection({
     host: 'tubook.cfsglt85tvze.us-east-1.rds.amazonaws.com',
-    user: '****',
-    password: '***',
-    database: '***',
-    port: ***
+    user: 'admin',
+    password: 'admin123',
+    database: 'tuBook',
+    port: 3306
   });
 
   connection.connect((err) => {
