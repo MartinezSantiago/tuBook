fec96a1e758eda001a96da382500f73fe4da3e45
Santiago Martinez <79525736+MartinezSantiago@users.noreply.github.com>
2023-05-23 18:46:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const app = express();
 const createConnection = require('./db');
 const bodyParser = require('body-parser');
 const uploadImage = require('./helper/imageHelper');
-
+const cors = require('cors');
 // Create the database connection
 const connection = createConnection();
-
+app.use(cors());
 // Middleware to attach the connection object and uploadImage function to the request
 app.use(bodyParser.json(), (req, res, next) => {
   req.dbConnection = connection;
   req.uploadImageToS3 = uploadImage;
   next();
 });
+
 
 // Import the routes
 const booksRouter = require('./routes/books');
