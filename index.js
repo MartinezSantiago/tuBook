e1aba8bf65604f089cc17ee63c264eda4f776d9f
Santiago Martinez <79525736+MartinezSantiago@users.noreply.github.com>
2023-05-24 19:59:08 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const cors = require('cors');
 // Create the database connection
 const connection = createConnection();
 app.use(cors());
-// Middleware to attach the connection object and uploadImage function to the request
-app.use(bodyParser.json(), (req, res, next) => {
+app.use(bodyParser.json());
+
+// Custom middleware to attach the connection object and uploadImage function to the request
+app.use((req, res, next) => {
   req.dbConnection = connection;
   req.uploadImageToS3 = uploadImage;
   next();
@@ -18,11 +20,12 @@ app.use(bodyParser.json(), (req, res, next) => {
 // Import the routes
 const booksRouter = require('./routes/books');
 const imagesRouter = require('./routes/images');
+const usersRouter = require('./routes/users');
 
 // Register routes
 app.use('/books', booksRouter);
 app.use('/images', imagesRouter);
-
+app.use('/users', usersRouter);
 // Start the server
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
