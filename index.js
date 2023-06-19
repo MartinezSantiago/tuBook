e7666c5b3cce1aa5729b446057cfd3b5e88fb79d
Santiago Martinez <79525736+MartinezSantiago@users.noreply.github.com>
2023-06-19 16:41:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const createConnection = require('./db');
 const bodyParser = require('body-parser');
 const uploadImage = require('./helper/imageHelper');
 const cors = require('cors');
+
 // Create the database connection
 const connection = createConnection();
 app.use(cors());
@@ -12,9 +13,8 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   req.dbConnection = connection;
   req.uploadImageToS3 = uploadImage;
+  next();
 });
-
-
 
 // Import the routes
 const booksRouter = require('./routes/books');
@@ -27,6 +27,7 @@ app.use('/books', booksRouter);
 app.use('/images', imagesRouter);
 app.use('/users', usersRouter);
 app.use('/loans', loansRouter);
+
 // Start the server
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
